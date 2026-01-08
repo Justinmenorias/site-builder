16ec79007c4ead944773fd350d6cd5866f936821
Justin Menorias <justinmenorias70@gmail.com>
2026-01-08 18:00:54 +0800

    feat: implement view page ui

--- frontend/src/pages/View.tsx
+++ frontend/src/pages/View.tsx
@@ -1,3 +1,55 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { dummyProjects } from '../assets/assets';
+import { Loader2Icon } from 'lucide-react';
+import ProjectPreview from '../components/ProjectPreview';
+import type { Project } from '../types';
+
 export default function View() {
-  return <div>View</div>;
+  const { projectId } = useParams();
+  const [code, setCode] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchCode = async () => {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+
+      const code = dummyProjects.find(
+        (project) => project.id === projectId
+      )?.current_code;
+
+      if (code) {
+        setCode(code);
+        setIsLoading(false);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCode();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader2Icon className="size-7 animate-spin text-indigo-200" />
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {code && (
+        <ProjectPreview
+          project={{ current_code: code } as Project}
+          isGenerating={false}
+          showEditorPanel={false}
+        />
+      )}
+    </div>
+  );
 }
